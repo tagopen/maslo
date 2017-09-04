7a9e545a26716941ae0b702c0872e3b9fbba8046
Security2431 <Artem2431@gmail.com>
2017-09-04 23:37:45 +0300

    section footer

--- app/js/main.js
+++ app/js/main.js
@@ -31,33 +31,47 @@
     $(".intro__img--2").addClass('active');
   });
 
-$('.tooltipstered').tooltipster({
-    contentCloning: true,
-    theme: 'tooltipster-shadow',
-    animation: 'fade',
-    delay: 50,
-    side: 'bottom',
-    maxWidth: 460
-});
+  $('.tooltipstered').tooltipster({
+      contentCloning: true,
+      theme: 'tooltipster-shadow',
+      animation: 'fade',
+      delay: 50,
+      side: 'bottom',
+      maxWidth: 460
+  });
+  $(function () {
 
-$('.tech__slider').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: true,
-  infinite: true,
-  focusOnSelect: true,
-  appendArrows: $('.tech__arrows'),
-  prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"><i class="ic ic--left"></i></button>',
-  nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"><i class="ic ic--right"></i></button>',
-  responsive: [
-  {
-    breakpoint: 1600,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1
+    
+      var $status       = $('.tech__count'),
+          $slickElement = $('.tech__slider');
+    if($slickElement.length) {
+      $slickElement.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+          //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+          var i = (currentSlide ? currentSlide : 0) + 1;
+          $status.text(i + '/' + slick.slideCount);
+      });
+
+      $slickElement.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        infinite: true,
+        focusOnSelect: true,
+        appendArrows: $('.tech__arrows'),
+        prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"><i class="ic ic--left"></i></button>',
+        nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"><i class="ic ic--right"></i></button>',
+        responsive: [
+          {
+            breakpoint: 1600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
     }
-  }
-  ]
-});
+
+  });
 
 })(jQuery); // End of use strict
